ad497d843a520d9e2c30caec97f56375487f2953
Zulfiqar Ali <zulfiqarali@alitechgrid.com>
2026-08-01 16:34:41 -0700

    Connect Zoho booking page

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -1,7 +1,7 @@
 window.ALITECHGRID_CONFIG = {
   // REQUIRED BEFORE PUBLIC LAUNCH:
   // Paste the complete Zoho Calendar "Live link" between the quotation marks.
-  bookingUrl: "PASTE_FULL_ZOHO_LIVE_LINK_HERE",
+  bookingUrl: "https://calendar.zohocloud.ca/zc/view/slot-booking/zz080112307961a55f27599c17ced0f5a1aab58c6916680163755acca06e9e3a51d1360c0b1025b7e3be1c0fdd1be5fcdca7762647",
 
   siteUrl: "https://alitechgrid.ca",
   contactEmail: "contact@alitechgrid.com",
